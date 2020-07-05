0f13f7c8d976e4700e42b55960acd9d3d1911d9b
KingricharDV#0439 <northerncommunity1@gmail.com>
2020-07-04 16:09:17 -1000

    update

--- src/astra/astratransaction.h
+++ src/astra/astratransaction.h
@@ -1,5 +1,5 @@
-#ifndef LUXTRANSACTION_H
-#define LUXTRANSACTION_H
+#ifndef ASTRATRANSACTION_H
+#define ASTRATRANSACTION_H
 
 #include <libethcore/Transaction.h>
 
@@ -43,16 +43,16 @@ struct VersionVM{
     }
 }__attribute__((__packed__));
 
-class LuxTransaction : public dev::eth::Transaction{
+class AstraTransaction : public dev::eth::Transaction{
 
 public:
 
-    LuxTransaction() : nVout(0) {}
+    AstraTransaction() : nVout(0) {}
 
-    LuxTransaction(dev::u256 const& _value, dev::u256 const& _gasPrice, dev::u256 const& _gas, dev::bytes const& _data, dev::u256 const& _nonce = dev::Invalid256):
+    AstraTransaction(dev::u256 const& _value, dev::u256 const& _gasPrice, dev::u256 const& _gas, dev::bytes const& _data, dev::u256 const& _nonce = dev::Invalid256):
 		dev::eth::Transaction(_value, _gasPrice, _gas, _data, _nonce) {}
 
-    LuxTransaction(dev::u256 const& _value, dev::u256 const& _gasPrice, dev::u256 const& _gas, dev::Address const& _dest, dev::bytes const& _data, dev::u256 const& _nonce = dev::Invalid256):
+    AstraTransaction(dev::u256 const& _value, dev::u256 const& _gasPrice, dev::u256 const& _gas, dev::Address const& _dest, dev::bytes const& _data, dev::u256 const& _nonce = dev::Invalid256):
 		dev::eth::Transaction(_value, _gasPrice, _gas, _dest, _data, _nonce) {}
 
     void setHashWith(const dev::h256 hash) { m_hashWith = hash; }
--- src/astra/storageresults.cpp
+++ src/astra/storageresults.cpp
@@ -1,4 +1,4 @@
-#include <lux/storageresults.h>
+#include <astra/storageresults.h>
 
 StorageResults::StorageResults(std::string const& _path){
 	path = _path + "/resultsDB";
