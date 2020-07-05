d1db81143ca5be0fe6051b00eb72d7e07a6ca25c
KingricharDV#0439 <northerncommunity1@gmail.com>
2020-07-05 09:00:48 -1000

    update

---
+++ src/qt/astranodeconfigdialog.h
@@ -0,0 +1,27 @@
+#ifndef ASTRANODECONFIGDIALOG_H
+#define ASTRANODECONFIGDIALOG_H
+
+#include <QDialog>
+
+namespace Ui {
+    class AstraNodeConfigDialog;
+}
+
+QT_BEGIN_NAMESPACE
+class QModelIndex;
+QT_END_NAMESPACE
+
+/** Dialog showing transaction details. */
+class AstraNodeConfigDialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit AstraNodeConfigDialog(QWidget *parent = 0, QString nodeAddress = "123.456.789.123:28666", QString privkey="MASTERNODEPRIVKEY");
+    ~AstraNodeConfigDialog();
+
+private:
+    Ui::AstraNodeConfigDialog *ui;
+};
+
+#endif // ASTRANodeConfigDialogNODECONFIGDIALOG_H
